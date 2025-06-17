d29223e46da827bb1a4ffb34328c966932df4381
Cybil Fatima <cybil.fatima@gmail.com>
2025-06-17 14:51:58 +0500

    Added assignment 3

---
+++ Assignments/Assignment_3/warehouse-app/hello.js
@@ -0,0 +1,2 @@
+console.log("Cybil Fatima");
+// console.log(window);
---
+++ Assignments/Assignment_3/warehouse-app/model/product.model.js
@@ -0,0 +1,12 @@
+
+let mongoose = require("mongoose");
+
+let productSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+});
+
+let ProductModel = mongoose.model("Product", productSchema);
+
+module.exports = ProductModel;
